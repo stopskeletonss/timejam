1f39d5c57c6e2e9d0144194449f934090c2a1d88
Eliana Marshall <elianagmarshall@gmail.com>
2025-03-21 18:32:11 -0400

    Add rewind mechanic
    - Update PlayerController to interpolate between the player's current position and stored position from 3 seconds prior
    - Add noAnimation to PlayerModel while rewinding

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,8 +4,16 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public Animator playerAnim;
-    public int speed;
+    public Animator playerAnim; // references the animator to control animations
+    public int speed; // speed of the player's movement
+
+    private Queue<Vector3> positions = new Queue<Vector3>(); // stores the player's previous positions
+    private Queue<float> times = new Queue<float>(); //stores the times for each stored position
+    public float storeDuration = 3f; // duration to store positions
+    public float rewindDuration = 1f; // duration of rewind mechanic
+
+    private bool rewinding = false; // flag to check if rewind is in progress
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,29 +23,63 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (!rewinding) // only allow button and mouse events to be triggered if rewind is not in progress
         {
-            playerAnim.Play("attack1");
+            if (Input.GetMouseButtonDown(0))
+            {
+                playerAnim.Play("attack1");
+            }
+            else if (Input.GetMouseButtonDown(1))
+            {
+                playerAnim.Play("attack2");
+            }
+            else if (Input.GetKey(KeyCode.A))
+            {
+                transform.position += Vector3.left * speed * Time.deltaTime; // move left
+                playerAnim.SetBool("isRunning", true);
+                playerAnim.transform.localScale = new Vector3(-2f, 2f, 1f); // flip facing direction to the left
+            }
+            else if (Input.GetKey(KeyCode.D))
+            {
+                transform.position += Vector3.right * speed * Time.deltaTime; // move right
+                playerAnim.SetBool("isRunning", true);
+                playerAnim.transform.localScale = new Vector3(2f, 2f, 1f); //flip facing direction to the right
+            }
+            else if (Input.GetKey(KeyCode.R))
+            {
+                Vector3 targetPosition = positions.Peek(); // get the oldest stored position
+                StartCoroutine(Rewind(targetPosition));
+                playerAnim.SetBool("isRewinding", true);
+            }
+            else
+            {
+                playerAnim.SetBool("isRunning", false);
+            }
         }
-        else if (Input.GetMouseButtonDown(1))
+
+        positions.Enqueue(transform.position); // stores the current position at every frame
+        times.Enqueue(Time.time); // stores the current time at every frame
+
+        while (times.Count > 0 && Time.time - times.Peek() > storeDuration) // remove positions and their corresponding times after they have been in the queue for 3 seconds
         {
-            playerAnim.Play("attack2");
+            positions.Dequeue();
+            times.Dequeue();
         }
-        else if (Input.GetKey(KeyCode.A))
+    }
+
+    IEnumerator Rewind(Vector3 targetPosition)
+    {
+        rewinding = true;
+        Vector3 startPosition = transform.position; // stores the current position before rewinding
+        float elapsedTime = 0f;
+
+        while (elapsedTime < rewindDuration)
         {
-            transform.position += Vector3.left * speed * Time.deltaTime;
-            playerAnim.SetBool("isRunning", true);
-            playerAnim.transform.localScale = new Vector3(-2f, 2f, 1f);
+            transform.position = Vector3.Lerp(startPosition, targetPosition, elapsedTime / rewindDuration); // interpolate the current position to the target position 
+            elapsedTime += Time.deltaTime;
+            yield return null;
         }
-        else if (Input.GetKey(KeyCode.D))
-        {
-            transform.position += Vector3.right * speed * Time.deltaTime;
-            playerAnim.SetBool("isRunning", true);
-            playerAnim.transform.localScale = new Vector3(2f, 2f, 1f);
-        }
-        else
-        {
-            playerAnim.SetBool("isRunning", false);
-        }
+        rewinding = false;
+        playerAnim.SetBool("isRewinding", false);
     }
 }
