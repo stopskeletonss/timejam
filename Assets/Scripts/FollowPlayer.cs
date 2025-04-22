c86bc3bb7eeed07962997075691ed3dbf40031ff
Kevin Pittman <kevinthe3rd_2005@hotmail.com>
2025-04-22 13:15:56 -0400

    Camera Variables
    Made camera distance values into public variables

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -7,18 +7,20 @@ public class NewBehaviourScript : MonoBehaviour
 {
     public Transform player;
     private int speed = 5;
+    public int followDistance;
+    public int followHeight;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        followDistance *= -1;
     }
 
     // Update is called once per frame
     void Update()
     {
         Vector3 startPosition = transform.position; // stores the current position
-        Vector3 playerPosition = player.transform.position + new Vector3(0, 4, -5); // get the position of the player
+        Vector3 playerPosition = player.transform.position + new Vector3(0, followHeight, followDistance); // get the position of the player
         transform.position = Vector3.Lerp(startPosition, playerPosition, Time.deltaTime*speed); // follow the player
     }
 }
